419c407268c207e9eaa7731e1b4a33f1815e28d0
HoodedArcher <charltoncohen@yahoo.com>
2019-04-26 12:13:07 -0500

    Coded the main()

--- CohenPayton002PA4/src/CohenPayton002PA4.java
+++ CohenPayton002PA4/src/CohenPayton002PA4.java
@@ -1,4 +1,16 @@
+/**
+ * @(#)Addressee.java
+ * @author Charlton Cohen
+ * @version 1.0 2019/4/25 8:00 PM
+ * Program Putpose: Prompts the user for their name and variables for an addresss book. Creates an address book for the user. Prompts the user to create entries for addressee's
+ * this information is stored in an array and when the user is done inputting addressee's the array is printed.
+ */ 
 public class CohenPayton002PA4
 {
-  
+  public static void main(String[] args)
+  {
+    AddressBook book = new AddressBook();
+    book.start();
+    System.exit(0);
+  }
 }
